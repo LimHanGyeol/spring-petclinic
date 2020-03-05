1918666c540b591ace8eb977d118b46ab0507692
LimHanGyeol <dlagksruf19@naver.com>
2020-03-05 15:53:34 +0900

    Section2. 스프링 IoC - 스프링 빈(Bean)

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -15,6 +15,7 @@
  */
 package org.springframework.samples.petclinic.owner;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.samples.petclinic.visit.VisitRepository;
 import org.springframework.stereotype.Controller;
@@ -39,24 +40,11 @@ class OwnerController {
 
 	private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM = "owners/createOrUpdateOwnerForm";
 
-	private final OwnerRepository owners;
+	@Autowired
+	private OwnerRepository owners;
 
+	@Autowired
 	private VisitRepository visits;
-
-	private final ApplicationContext applicationContext;
-
-	public OwnerController(OwnerRepository clinicService, VisitRepository visits, ApplicationContext applicationContext) {
-		this.owners = clinicService;
-		this.visits = visits;
-		this.applicationContext = applicationContext;
-	}
-
-	@GetMapping("/bean")
-    @ResponseBody
-    public String getBean() {
-	    return "bean : " +applicationContext.getBean(OwnerRepository.class) + "\n"
-            + "owners : " + this.owners;
-    }
 
 	@InitBinder
 	public void setAllowedFields(WebDataBinder dataBinder) {
